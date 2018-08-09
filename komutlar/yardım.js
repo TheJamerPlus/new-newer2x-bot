f5916f5156e994fd87c38d2ad99e8b51aacc4159
TheJamerPlus <42062274+TheJamerPlus@users.noreply.github.com>
2018-08-09 13:10:37 +0300

    Update yardım.js

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -10,7 +10,7 @@ exports.run = (client, message, params) => {
   .setColor(0x00ffff)
   .addField("**Eğlence ve Kullanıcı Komutları:**", `n!banned = Dene ve Gör! \nn!avatarım = Avatarınınızı Gösterir. \nn!herkesebendençay = Herkese Çay Alırsınız. \nn!koş = Koşarsınız.\nn!çayiç = Çay İçersiniz. \nn!çekiç = İstediğiniz Kişiye Çekiç Atarsınız. \nn!çayaşekerat = Çaya Şeker Atarsınız. \nn!yumruh-at = Yumruk Atarsınız. \nn!yaz = Bota İstediğiniz Şeyi Yazdırırsınız. \nn!sunucuresmi = BOT Sunucunun Resmini Atar. \nn!sunucubilgi = BOT Sunucu Hakkında Bilgi Verir. \nn!kullanıcıbilgim = Sizin Hakkınızda Bilgi Verir. `)
   .addField("**Yetkilisi Komutlar**", `n!ban = İstediğiniz Kişiyi Sunucudan Banlar. \nn!kick  = İstediğiniz Kişiyi Sunucudan Atar. \nn!unban = İstediğiniz Kişinin Yasağını Açar. \nn!sustur = İstediğiniz Kişiyi Susturur. \nn!oylama = Oylama Açar. \nn!duyuru = Güzel Bir Duyuru Görünümü Sağlar.`)
-  .addField("**Ana Komutlar**", "h!yardım = BOT Komutlarını Atar. \nn!bilgi = BOT Kendisi Hakkında Bilgi Verir. \nn!ping = BOT Gecikme Süresini Söyler. \nn!davet = BOT Davet Linkini Atar. \nn!istatistik = BOT İstatistiklerini Atar.")
+  .addField("**Ana Komutlar**", "h!yardım = BOT Komutlarını Atar. \nn!bilgi = BOT Kendisi Hakkında Bilgi Verir. \nn!ping = BOT Gecikme Süresini Söyler. \nn!davet = BOT Davet Linkini Atar. \nn!istatistik = BOT İstatistiklerini Atar. \nn!sunucuonline = Sunucuda Kaç Kişi Var")
   .addField("**Yapımcı**", " **@💻 NWR2X Helado Módem 💻#3812** ")
   .setFooter('**--------------------------**')
   if (!params[0]) {
